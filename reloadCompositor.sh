0be53a9e0983f13d84a84aeda27eff7188255e76
Tao Ren <tao@swarmnyc.com>
2016-11-17 16:37:19 +0000

    Update reloadCompositor.sh to copy so to kms-elements if exist.

--- reloadCompositor.sh
+++ reloadCompositor.sh
@@ -2,6 +2,13 @@
 sudo service kurento-media-server-6.0 stop
 cd gst/compositor
 make
-sudo cp .libs/libgstcompositor.so /usr/lib/x86_64-linux-gnu/gstreamer-1.5/
+SYSTEM_COMPOSITOR=`dpkg  -L gstreamer1.5-plugins-bad | grep libgstcompositor.so`
+sudo cp .libs/libgstcompositor.so ${SYSTEM_COMPOSITOR}
+echo Copy libgstcompositor.so to ${SYSTEM_COMPOSITOR}
+md5sum .libs/libgstcompositor.so
+md5sum ${SYSTEM_COMPOSITOR}
+KMS_ELEMENT_DIST=../../../kms-elements/gst-plugins-bad
+[ -e ${KMS_ELEMENT_DIST}/libgstcompositor.so ] && cp .libs/libgstcompositor.so ${KMS_ELEMENT_DIST}/libgstcompositor.so && echo Also copy libgstcompositor.so to ${KMS_ELEMENT_DIST} && md5sum ${KMS_ELEMENT_DIST}/libgstcompositor.so
+
 cd ../..
 sudo service kurento-media-server-6.0 start
